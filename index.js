444d0ab41f16bbab95e8f40b84aa34679c4b676a
Kenneth <wongyxk@hotmail.com>
2026-04-10 22:40:25 +0800

    fixed tiles conditions

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ const diceBtn = document.querySelector("#dice-btn");
 const dice1 = document.querySelector("#dice-1");
 const dice2 = document.querySelector("#dice-2");
 const sumText = document.querySelector("#sum");
+const tiles = document.querySelectorAll(".tiles");
+const submitBtn = document.querySelector(".submit-btn");
+
+const board = [];
+let isRunning = false;
+let isWinner = false;
+let sum = "";
 
 function showScreen(screenName) {
   const screens = [menuScreen, rulesScreen, gameScreen, resultScreen].filter(
@@ -37,18 +44,35 @@ rulesBtn.addEventListener("click", () => {
 });
 
 function rollDice() {
-  let randomDiceIdx = Math.ceil(Math.random() * 6);
-  let randomDiceIdx1 = Math.ceil(Math.random() * 6);
-  let sum = "";
-
-  let dice1Value = randomDiceIdx;
-  let dice2Value = randomDiceIdx1;
+  let dice1Value = Math.ceil(Math.random() * 6);
+  let dice2Value = Math.ceil(Math.random() * 6);
 
   sum = dice1Value + dice2Value;
 
   dice1.src = `images/inverted-dice-${dice1Value}.png`;
   dice2.src = `images/inverted-dice-${dice2Value}.png`;
-  sumText.textContent = `${sum}`;
+  sumText.innerText = `You rolled ${sum}`;
+
+  board.length = 0;
 }
 
 diceBtn.addEventListener("click", rollDice);
+
+tiles.forEach((tile) => {
+  tile.addEventListener("click", updateTile);
+});
+
+function updateTile(e) {
+  const value = Number(e.target.innerText);
+
+  if (board.includes(value)) {
+    board.splice(board.indexOf(value), 1);
+    e.target.classList.remove("selected");
+    return;
+  }
+
+  if (board.length === 2) return;
+
+  board.push(value);
+  e.target.classList.add("selected");
+}
